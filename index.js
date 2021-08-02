abbc46a51cdc475bc8a9dacdf2f15a9e56d31f65
Matheus Pessoa <matheuspessoafonseca@gmail.com>
2021-08-02 01:31:54 -0300

    Modifica o estilo dos botoes, substitui botao de decrement pelo save e cria funcoes javascript para os botoes de increment e de save

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
+// todo: Desenvolver função para aumentar a temperatura da cor do texto de #count-el à medida que se clica no botão de increment
+// todo: Desenvolver função que reinicia a temperatura da cor do texto todas as vezes que o botão de save for pressionado
+
 let count = 0;
 
-console.log(count);
+let countEl = document.getElementById("count-el");
+/**                                            L--------> The DOM: Document Object Model (aka how you use JavaScript
+ * JS, please store the text of the element with the id                                   to modify a website)
+ */
+
+function increment() {
+  count += 1;
+  countEl.innerText = count;
+  // JS, please update its text to the value of the variable count
+}
+
+function save() {
+  console.log(count);
+}
