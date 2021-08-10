231d8992a7e0c40fa0822becf0b9f1d0d139fe34
Matheus Pessoa <matheuspessoafonseca@gmail.com>
2021-08-10 00:09:09 -0300

    Refatora funçao resetAll

--- index.js
+++ index.js
@@ -1,23 +1,16 @@
-// todo: Desenvolver função para aumentar a temperatura da cor do texto de #count-el à medida que se clica no botão de increment
-// todo: Desenvolver função que reinicia a temperatura da cor do texto todas as vezes que o botão de save for pressionado
-
-let count = 0;
-
 let countEl = document.getElementById("count-el");
-/**                                            L--------> The DOM: Document Object Model (aka how you use JavaScript
- * JS, please store the text of the element with the id                                   to modify a website)
- */
+let count = 0;
 
 function increment() {
   count += 1;
-  countEl.textContent = count; // innerText struggles to get non-readable human text
-  // JS, please update its text to the value of the variable count
+  countEl.textContent = count;
 }
 
 let saveEl = document.getElementById("save-el");
+
 function save() {
-  let previousEntry = count + " - ";
-  saveEl.textContent += previousEntry;
+  let previousEntries = count + " - ";
+  saveEl.textContent += previousEntries;
 }
 
 function resetCounter() {
@@ -25,8 +18,7 @@ function resetCounter() {
   count = 0;
 }
 
-function resetAll(){
-  countEl.textContent = 0;
-  count = 0;
+function resetAll() {
+  resetCounter();
   saveEl.textContent = "";
 }
