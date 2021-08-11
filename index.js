77bcea59c910ea91da5b49d69f9f0fc887730351
Matheus Pessoa <matheuspessoafonseca@gmail.com>
2021-08-11 15:53:12 -0300

    Refatora eventos (adiciona event listener no lugar da versao inline com o onclick)

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 let countEl = document.getElementById("count-el");
+let incrementButton = document.getElementById("increment-btn");
+incrementButton.addEventListener("click", increment);
+
 let count = 0;
 
 function increment() {
@@ -7,16 +10,24 @@ function increment() {
 }
 
 let saveEl = document.getElementById("save-el");
+let saveButton = document.getElementById("save-btn");
+saveButton.addEventListener("click", save);
 
 function save() {
   let previousEntries = count + " - ";
   saveEl.textContent += previousEntries;
 }
 
+let resetCounterButton = document.getElementById("reset-counter-btn");
+resetCounterButton.addEventListener("click", resetCounter);
+
 function resetCounter() {
   countEl.textContent = 0;
   count = 0;
 }
+
+let resetAllButton = document.getElementById("reset-all-btn");
+resetAllButton.addEventListener("click", resetAll);
 
 function resetAll() {
   resetCounter();
